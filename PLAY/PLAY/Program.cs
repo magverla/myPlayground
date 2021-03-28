bb9ea3a73810042a269d7f6c02b6202a3604f71a
magverla <magverla@gmail.com>
2021-03-28 16:22:04 +0200

    1

--- PLAY/PLAY/Program.cs
+++ PLAY/PLAY/Program.cs
@@ -10,7 +10,7 @@ namespace PLAY
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
