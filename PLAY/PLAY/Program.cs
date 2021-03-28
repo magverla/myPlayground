fdee99c794d1e03574e28d68dc63c528321b14cc
magverla <magverla@gmail.com>
2021-03-28 15:40:05 +0200

    commit

---
+++ PLAY/PLAY/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PLAY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
