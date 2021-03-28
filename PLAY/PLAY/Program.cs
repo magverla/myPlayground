da0602adc4308bd0592518374643313c16fcfef2
magverla <magverla@gmail.com>
2021-03-28 21:41:41 +0200

    learning

--- PLAY/PLAY/Program.cs
+++ PLAY/PLAY/Program.cs
@@ -12,9 +12,24 @@ namespace PLAY
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Wie heisst du?");
-            var name = Console.ReadLine();
-            Console.WriteLine("Hallo " + name);
-            Console.ReadLine();
+            var vorname = Console.ReadLine();
+            var nachname = Console.ReadLine();
+            Console.WriteLine("Hallo {2} {1} {0}",vorname,nachname,vorname+nachname);
+
+            Console.WriteLine("Gib eine Ganze Zahl ein");
+            var number = Console.ReadLine();
+            int num;
+            try
+            {
+                num = Int32.Parse(number);
+            }catch (FormatException e)
+            {
+                Console.WriteLine("keine ganze Zahl ->");
+                Console.WriteLine(e);
+            }
+            
+
+            
         }
     }
 }
---
+++ PLAY/PLAY/Taschenrechner/Taschenrechner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PLAY.Taschenrechner
+{
+
+    class Taschenrechner
+    {
+        int number1;
+        int number2;
+        float fnumber1;
+        float fnumber2;
+
+        Taschenrechner (String Zahl1, String operation, String Zahl2)
+        {
+            
+        }
+
+    }
+}
