c0b50151b207399cdca1c33a31d005199a067642
magverla <magverla@gmail.com>
2021-03-28 22:22:31 +0200

    calc with float

---
+++ PLAY/PLAY/Taschenrechner/MitFloatRechnen/CalcWithFloat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PLAY.Taschenrechner.MitFloatRechnen
+{
+    class CalcWithFloat
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
