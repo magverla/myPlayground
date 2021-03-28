9b99371d7da141c945ef1b0e43a55416c2cbe683
magverla <magverla@gmail.com>
2021-03-28 17:31:41 +0200

    Revert "commit"
    This reverts commit 13869e879fa1cee7985dd8fd4034a55b85e27e6f.

--- PLAY/PLAY/Program.cs
+++ PLAY/PLAY/Program.cs
@@ -11,10 +11,8 @@ namespace PLAY
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Wer bist du?");
-            var name = Console.ReadLine();
-            Console.WriteLine("Hallo " + name);
-            Console.ReadLine();
+            Console.WriteLine("Anwendung schließt nach drücken von 'Enter'");
+            Console.WriteLine();
         }
     }
 }
